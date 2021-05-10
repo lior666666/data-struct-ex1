1280f21645dae21c227aa65cee78fb939ac6e9a8
Lior Bar <liorbar03@gmail.com>
2021-05-10 15:27:29 +0300

    update 2 classes for 2 different trees

---
+++ model.h
@@ -0,0 +1,53 @@
+#ifndef MODEL_H_
+#define MODEL_H_
+#include <iostream>
+
+class Model
+{
+    int typeID;
+    int modelID;
+    int score;
+    int num_of_sales;
+    int num_of_complaints;
+public:
+    Model() = default;
+    ~Model() = default;
+    Model(const Model& model) = default;
+    friend bool operator<(const Model& model1, const Model& model2);
+    Model(int typeID, int modelID)
+    {
+        this->typeID = typeID;
+        this->modelID = modelID;
+        this->score = 0;
+        this->num_of_sales = 0;
+        this->num_of_complaints = 0;
+    }
+    void addSale()
+    {
+        this->num_of_sales = this->num_of_sales++;
+        this->score = this->score+10;
+    }
+    void addComplain(int months)
+    {
+        this->num_of_complaints = this->num_of_complaints++;
+        this->score = this->score-(100/months);
+    }
+    int getScore()
+    {
+        return this->score;
+    }
+};
+
+bool operator<(const Model& model1, const Model& model2)
+{
+    if(model1.score == model2.score)
+    {
+        if(model1.typeID == model2.typeID)
+        {
+            return model1.modelID < model2.modelID;
+        }
+        return model1.typeID < model2.typeID;
+    }
+    return model1.score < model2.score;
+}
+#endif
---
+++ models_array.h
@@ -0,0 +1,32 @@
+#ifndef MODELS_ARRAY_H_
+#define MODELS_ARRAY_H_
+#include "model.h"
+#include <iostream>
+
+class ModelsArray
+{
+    int typeID;
+    int num_of_models;
+    int max_sales;
+    Model* best_seller_model;
+    Model* array;
+public:
+    ModelsArray() = default;
+    ~ModelsArray() = default;
+    ModelsArray(const ModelsArray& models_array1) = default;
+    //check arg before calling the constructor!!!
+    ModelsArray(int typeID, int num_of_models)
+    {
+        Model* array = new Model[num_of_models];
+        for (int i = 0; i < num_of_models; i++)
+        {
+            array[i] = *(new Model(typeID, i));
+        }
+        this->typeID = typeID;
+        this->num_of_models = num_of_models;
+        this->max_sales = 0; // there is no sales when allocation
+        this->best_seller_model = &(array[0]); // every typeID has at least one model
+        this->array = array;
+    };
+};
+#endif
