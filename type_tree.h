e126bc85e63f890ce756c09fa0eb8663c1923dc6
liorbar03 <51298360+liorbar03@users.noreply.github.com>
2021-05-24 01:13:56 +0300

    Update type_tree.h

--- type_tree.h
+++ type_tree.h
@@ -7,21 +7,19 @@
 class TypeTree
 {
     int typeID;
-    int number_of_models;
     AvlTree<Model>* models_tree;
 public:
-    TypeTree(): typeID(0), number_of_models(0), models_tree() {}
+
+    TypeTree(): typeID(0), models_tree() {}
 
     friend bool operator==(const TypeTree& type_tree1, const TypeTree& type_tree2);
 
     friend bool operator<(const TypeTree& type_tree1, const TypeTree& type_tree2);
 
-    friend std::ostream& operator<<(std::ostream& os, const TypeTree& c);
-
     ~TypeTree() = default;
 
-    TypeTree(int typeID, int number_of_models, AvlTree<Model>* models_tree) // if you want to make dummy, just put NULL into models_tree
-    : typeID(typeID), number_of_models(number_of_models), models_tree()
+    TypeTree(int typeID, AvlTree<Model>* models_tree) // if you want to make dummy, just put NULL into models_tree
+    : typeID(typeID), models_tree()
     {
         this->models_tree = models_tree; 
     }
@@ -29,26 +27,6 @@ public:
     int getTypeID()
     {
         return this->typeID;
-    }
-
-    bool insertModel(Model model)
-    {
-        bool is_inserted = this->models_tree->insertElement(model);
-        if (is_inserted)
-        {
-            this->number_of_models++;
-        }
-        return is_inserted;
-    }
-
-    bool removeModel(Model model)
-    {
-        bool is_remove = this->models_tree->removeElement(model);
-        if (is_remove)
-        {
-            this->number_of_models--;
-        }
-        return is_remove;
     }
 
     AvlTree<Model>* getModelsTree()
@@ -59,11 +37,6 @@ public:
     AvlTree<Model>* getLowestModel()
     {
         return this->models_tree->getMinNode();
-    }
-
-    int getModelsTreeLength()
-    {
-        return this->number_of_models;
     }
 };
 
@@ -82,8 +55,4 @@ bool operator>(const TypeTree& type_tree1, const TypeTree& type_tree2)
     return !(type_tree1 < type_tree2) && !(type_tree1 == type_tree2);
 }
 
-std::ostream& operator<<(std::ostream& os, const TypeTree& c)
-{
-    return os << "typeTree:" << c.typeID << ":" << c.number_of_models << "***";
-}
 #endif
